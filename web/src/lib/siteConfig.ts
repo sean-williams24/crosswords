4b6334f8b6576f64c40200a2664f2573dd200137
Sean Williams <sean.r.williams85@gmail.com>
2026-07-09 22:45:22 +0100

    Amend website URL

--- web/src/lib/siteConfig.ts
+++ web/src/lib/siteConfig.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   appName: "Backword",
   tagline: "A word game that works backwards.",
-  appStoreUrl: "https://apps.apple.com/gb/app/backword/id6773428497",
+  appStoreUrl: "https://apps.apple.com/app/backword/id6773428497",
   developerName: "Sean Williams",
   supportEmail: "wilmslo@gmail.com",
   lastUpdated: "June 16, 2026",
